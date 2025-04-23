04580b776e4d5877acbd0aebe97175121f4d24d1
GARY GRAS <gary.gras07@gmail.com>
2025-04-23 11:07:15 +0200

    feat: create footer with css

--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -3,24 +3,20 @@ import logo from "../assets/images/logo-codexart-sans-bg.png";
 import "../styles/footer.css";
 
 export default function Footer() {
-  return (
-    <ul>
-      <Link to="/" className="logoFooter">
-        <img
-          src={logo}
-          alt="logo, forme géométrique avec l'inscription Codex Art."
-        />
-      </Link>
-      <li>&copy; 2025 - Codex Art </li>
-      <div>
-        <li>Mentions légales</li> -{" "}
-        <li>
-          <Link to="https://www.economie.gouv.fr/politique-confidentialite">
-            {" "}
-            Politique de confidentialité
-          </Link>
-        </li>
-      </div>
-    </ul>
-  );
+    return (
+        <footer>
+            <Link to="/" className="logoFooter">
+                <img src={logo} alt="logo, forme géométrique avec l'inscription Codex Art." />
+            </Link>
+            <p>&copy; 2025 - Codex Art </p>
+            <div>
+                <span>Mentions légales</span> -
+                <span>
+                    <Link to="https://www.economie.gouv.fr/politique-confidentialite">
+                        Politique de confidentialité
+                    </Link>
+                </span>
+            </div>
+        </footer>
+    );
 }
