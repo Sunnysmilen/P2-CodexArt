fedd66e8b857f87fc84096ff745765a60ab48e92
NabilY-5 <nabilyahya59280@gmail.com>
2025-04-23 11:49:42 +0200

    fix: route created for homepage and navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import { Outlet } from "react-router";
+import { Navbar } from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <main>
         <Outlet />
       </main>
